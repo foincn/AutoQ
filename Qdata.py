827d522c688a5d2246003b12859a7e9960e84d8e
tz1006 <a@omg.tf>
2019-01-13 17:37:14 +0800

    Update Qdata.py

--- Qdata.py
+++ Qdata.py
@@ -11,6 +11,15 @@ pd.options.mode.chained_assignment = None
 
 
 from datetime import datetime, timedelta
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+import futuquant as ft
+import pandas as pd
+pd.options.mode.chained_assignment = None
+
+
+
+from datetime import datetime, timedelta
 
 q = ft.OpenQuoteContext(host='sz.omg.tf', port=11111)
 
@@ -30,21 +39,32 @@ def datechange(date, change):
 
 
 def trading_day(date, change=0):
-    startdate = datechange(date, -50)
-    tradingdatelist = q.get_trading_days('SH', start=startdate, end=date)[1]
-    tradingdate = tradingdatelist[-1-change]
+    if change <= 0:
+        c = ((-change // 4) + 1) * 7
+        startdate = datechange(date, -c)
+        enddate=date
+    else:
+        c = ((change // 4) + 1) * 7
+        startdate = datechange(date, 1)
+        enddate = datechange(date, c)
+    tradingdatelist = q.get_trading_days('SH', start=startdate, end=enddate)[1]
+    if change <= 0:
+        tradingdate = tradingdatelist[-1-change]
+    else:
+        tradingdate = tradingdatelist[change-1]
     return tradingdate
 
 
+
 def Qdata(ftcode, date, day=15, future=False):
-    change = day + 19
+    change = - (day + 19)
     start_date = trading_day(date, change=change)
     end_date = trading_day(date)
     data = q.get_history_kline(ftcode, start=start_date, end=end_date, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ)[1]
-    if len(data) != change:
-        print(1)
+    if len(data) != -change:
+        # print(1)
         start_date = datechange(date, -300)
-        data = q.get_history_kline(ftcode, start=start_date, end=end_date, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ)[1][-change:]
+        data = q.get_history_kline(ftcode, start=start_date, end=end_date, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ)[1][change:]
     data['ma5'] = data['close'].rolling(5).mean()
     data['ma10'] = data['close'].rolling(10).mean()
     data['ma15'] = data['close'].rolling(15).mean()
@@ -54,9 +74,18 @@ def Qdata(ftcode, date, day=15, future=False):
 
 
 
-def mod1(data, before=8, after=0):
+def mod0(data):
+    result = False
+    d1ma5 = data.iloc[-1]['ma5']
+    d1close = data.iloc[-1]['close']
+    if d1close < d1ma5:
+        result = True
+    return result
+
+
+def mod1(data, before=7, after=1):
     result = True
-    for d in range(after, before):
+    for d in range(after-1, before+1):
         i = - (d + 1)
         ma5 = data.iloc[i]['ma5']
         ma10 = data.iloc[i]['ma10']
@@ -69,35 +98,6 @@ def mod1(data, before=8, after=0):
 
 
 
-
-
-
-def datechange(date, change):
-    d = datetime.strptime(date,'%Y-%m-%d').date()
-    new_d = d + timedelta(days=change)
-    new_date = new_d.strftime('%Y-%m-%d')
-    return new_date
-
-
-def trading_day(date):
-    startdate = datechange(date, -5)
-    tradingdate = q.get_trading_days('SH', start=startdate, end=date)[1][-1]
-    return tradingdate
-
-
-def Qdata(ftcode, date, future=False):
-    start_date = datechange(date, -100)
-    data = q.get_history_kline(ftcode, start=start_date, end=date, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ)[1]
-    data['ma5'] = data['close'].rolling(5).mean()
-    data['ma10'] = data['close'].rolling(10).mean()
-    data['ma15'] = data['close'].rolling(15).mean()
-    data['ma20'] = data['close'].rolling(20).mean()
-    return data
-
-
-
-
-
 def mod2(data):
     d1ma5 = data.iloc[-1]['ma5']
     d2ma5 = data.iloc[-2]['ma5']
@@ -107,7 +107,18 @@ def mod2(data):
     return result
 
 
+def mod3(data):
+    result = False
+    d1ma5 = data.iloc[-1]['ma5']
+    d1ma10 = data.iloc[-1]['ma10']
+    d1ma15 = data.iloc[-1]['ma15']
+    d1ma20 = data.iloc[-1]['ma20']
+    if d1ma5 > d1ma10 and d1ma5 > d1ma15 and d1ma5 > d1ma20: 
+        result = True
+    return result
 
+    
+    
 
 
 
@@ -139,7 +150,7 @@ df = pd.DataFrame(columns=['code', 'date']
 class model4ft():
     def __init__(self, date, day=8):
         self.date = trading_day(date)
-        self.day = day
+        self.day = dayz
         self.df = pd.DataFrame(columns=['code', 'date'])
     def model4(self, code, select_date):
         start_date = datechange(select_date, -45)
@@ -181,7 +192,7 @@ class model5ft():
         start_date = datechange(select_date, -45)
         data = Qdata(code, select_date)
         d0 = data.iloc[-1]['time_key'].split(' ')[0]
-        if mod4(data):
+        if mod1(data, 14, 6) and mod3(data) and mod0(data):
             self.df.loc[len(self.df)] = [code, d0]
         else:
             pass
@@ -201,8 +212,12 @@ class model5ft():
 
 
 
-a = model5ft('2019-01-02')
+a = model5ft('2019-01-04')
 a.analyze()
 
+
                   
+
+                  
+              
                   
